3800d9c25f0c5cc1a2e8c3e51b1bfead255cd459
dylankress <dylankress@gmail.com>
2025-05-30 00:37:43 -0500

    very small tweaks... prepping for process_awaiting_upload_list method

--- src/wormhole_simulator/file_manager.py
+++ src/wormhole_simulator/file_manager.py
@@ -25,6 +25,7 @@ class FileManager:
             self.awaiting_upload.add(chunk_id)
 
     def process_awaiting_upload_list(self):
+        # This method should operate at a frequency we set in config.py called PROCESS_AWAITING_UPLOAD_LIST_FREQUENCY. Every time it runs it's goal is to move all the chunks in that list into self.awaiting_replication. In order to do this we need a few things for every chunk on the list: the chunk's owner and their upload_speed (use chunk_to_file_map for this), an online node we can send the file to (must be online, must have enough available storage space, would be best if they have a good uptime score, would be best if they have a good download speed). Once we've found a suitable host node we need to simulate this "transfer" by actually delaying moving chunks from awaiting_upload to awaiting_replication over the number of ticks it would take based on the lower of the 2 transfer speeds (upload_speed for uploading node or download_speed for host node). Then once that transfer is complete we need to either update chunk_to_file_map to keep track of where the uploaded chunk is being stored or add it to another Dict if there's a better way to keep track of it beyond what we already have built out here.
         pass
 
     def process_awaiting_replication_list(self):
